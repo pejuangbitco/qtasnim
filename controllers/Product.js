b95b592a225c7648f32850fea476bebb993c43c7
pejuangbitco <irsyadeinstein@gmail.com>
2021-05-26 06:33:09 +0700

    api crud product

--- controllers/Product.js
+++ controllers/Product.js
@@ -4,10 +4,86 @@ module.exports = {
   list: async (req, res) => {
     try {
       const result = await db.Product.findAll();
-      console.log(result);
       return res.send(result);
     } catch (error) {
       console.error(error);
     }  
-  }
+  },
+  
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await db.Product.findByPk(id);
+      return res.send(result);
+    } catch (error) {
+      console.error(error);
+    }  
+  },
+
+  create: async (req, res) => {
+    try {
+      const name = req.body.name;
+      const stock = req.body.stock;
+      const productType = req.body.productType;
+      
+      const result = await db.Product.create({
+        name: name,
+        stock: stock,
+        productType: productType,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+      return res.send(result);
+    } catch (error) {
+      console.error(error);
+    }  
+  },
+
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const name = req.body.name;
+      const stock = req.body.stock;
+      const productType = req.body.productType;
+      
+      // await db.Product.update({
+      //   name: name,
+      //   stock: stock,
+      //   productType: productType,
+      //   updatedAt: new Date(),
+      // }, {
+      //   where: {
+      //     id
+      //   }
+      // });
+
+      const data = await db.Product.findByPk(id);
+      if (data === null) {
+        return res.res.sendStatus(404).send('Resource not found');
+      }
+      data.name = name;
+      data.stock = stock;
+      data.productType = productType;
+      data.updatedAt = new Date();
+      data.save();
+
+      return res.send({
+        id
+      });
+    } catch (error) {
+      console.error(error);
+    }  
+  },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      await db.Product.destroy({
+        where: { id }
+      });
+      return res.send('Resource deleted');
+    } catch (error) {
+      console.error(error);
+    }  
+  },
 }
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,11 @@ const product = require('../controllers/Product');
 
 module.exports = (router) => {
   router.get('/ping', test.ping);
+  
   router.get('/product', product.list);
+  router.get('/product/:id', product.get);
+  router.post('/product', product.create);
+  router.put('/product/:id', product.update);
+  router.delete('/product/:id', product.delete);
   return router;
 }
